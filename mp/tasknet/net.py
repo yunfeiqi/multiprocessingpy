9e242beab557c8cc3ff500523bc45fc77aceb170
sam <qiyunfei@mininglamp.com>
2020-09-07 11:53:17 +0800

    'format'

--- mp/tasknet/net.py
+++ mp/tasknet/net.py
@@ -5,9 +5,8 @@
 @Version        :1.0
 '''
 
-from mp.operator import Operator
 from mp.utils.queueutil import create_queue_list
-import math
+
 
 class Net(object):
     def __init__(self):
@@ -15,44 +14,43 @@ class Net(object):
         self.layes = []
         self.relations = []
 
-    def reserve(self,left,right,mapping):
+    def reserve(self, left, right, mapping):
         # reserve structure of net
 
         # only first
-        if len(self.layes) <=0:
+        if len(self.layes) <= 0:
             self.layes.append(left)
         self.layes.append(right)
 
-        # reserve relations 
+        # reserve relations
         self.relations.append(mapping)
-        
+
     def display_net(self):
         for mapping in self.relations:
             print(mapping)
 
-    def hash_relation(self,left: list, right: list,capacity=50):
+    def hash_relation(self, left: list, right: list, capacity=50):
         left_cnt = len(left)
         right_cnt = len(right)
 
-        queue_cnt = min(left_cnt,right_cnt)
-        queues = create_queue_list(left_cnt,capacity)
+        queue_cnt = min(left_cnt, right_cnt)
+        queues = create_queue_list(left_cnt, capacity)
 
-        # mapping 
+        # mapping
         left_mapping = [id % queue_cnt for id in range(left_cnt)]
         right_mapping = [id % queue_cnt for id in range(right_cnt)]
 
         # set left side queue
-        for id,ope in enumerate(left):
+        for id, ope in enumerate(left):
             q_id = left_mapping[id]
             queue = queues[q_id]
             ope.set_output_queue(queue)
             ope.set_tail_num(right_cnt)
 
         # set right side queue
-        for id,ope in enumerate(right):
+        for id, ope in enumerate(right):
             q_id = right_mapping[id]
             queue = queues[q_id]
             ope.set_input_queue(queue)
 
-        self.reserve(left,right,left_mapping)
-
+        self.reserve(left, right, left_mapping)
